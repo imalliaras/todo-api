f019b1b9cb61fe978af95f43309d2d7b726c3081
Gmall <imalliaras@gmail.com>
2018-10-21 13:29:12 +0300

    Added GET with id parameter route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -30,6 +31,25 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((result) => {
+        if (!result) {
+            return res.status(404).send();
+        }
+        res.send({result});
+    }, (error) => {
+        res.status(400).send();
+    });
+    
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
