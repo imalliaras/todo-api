1378178c35e11c6dbf8a3c338739a6d53b3180fc
imalliaras <imalliaras@open.gr>
2018-10-25 11:33:52 +0300

    Addedd POST users login route

--- server/models/user.js
+++ server/models/user.js
@@ -77,6 +77,26 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    const User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (!result) {
+                    reject();
+                }
+                resolve(user);
+            })
+        });
+    });
+
+};
+
 UserSchema.pre('save', function(next) {
     var user = this;
 
